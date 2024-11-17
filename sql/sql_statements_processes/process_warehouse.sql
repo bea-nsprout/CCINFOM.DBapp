f857293d5f4f08e8afeaab8c4010e60c4abe1cd8
beansprout <150442539+bea-nsprout@users.noreply.github.com>
2024-11-17 15:46:49 +0800

    DONE warehouse commands

--- sql/sql_statements_processes/process_warehouse.sql
+++ sql/sql_statements_processes/process_warehouse.sql
@@ -2,24 +2,30 @@
 -- WAREHOUSE TABLE COMMANDS
 
 -- CREATE: new warehouse
-	-- check first if item exists in item mast
+	-- check first if warehouse (name and loc) exists in the warehouse table
 		SELECT EXISTS (
 			SELECT 1
-			FROM item_masterlist items
-			WHERE items.item_code = '0001010000TEMP036Y'
-		) AS item_exists;
+			FROM warehouse
+			WHERE warehouse_name = 'insertstringname' AND location = 'insertstringlocation'
+		) AS warehouse_exists;
         
 			-- if return 0: can add
             -- if return 1: do not add (warn the user), because that record already exists
 
 
-    -- insert new item into the database
-        INSERT INTO item_masterlist (item_code, item_desc, unit) VALUES
-		('0001010000TEMP036Y', 'TEST OR TEMP RECORD 36 YDS', 'ROLL'); -- 	replace 	
+    -- insert new warehouse into database
+        INSERT INTO warehouse (warehouse_name, location) VALUES
+		('insertstringname', 'insertstringlocation'); -- 	replace 	
 		
     -- update the warehouse inventory
         INSERT INTO warehouse_inventory (item_code, warehouse_id)
-		SELECT '0001010000TEMP036Y', warehouse_id
-		FROM warehouse;
+		SELECT item_code, 'insertstringname'
+		FROM item_masterlist;
         
-        -- this is a nested query that will introduce the new item to all the rows
+        -- this is a nested query that will introduce the new warehouse and setup all the possible items
+        
+-- MODIFY
+	-- COPY PASTE EXISTS CHECKER ABOVE: checking if warehouse name and loc exists in the table
+    UPDATE warehouse
+    SET warehouse_name = 'insertstringnameNEW', location = 'insertstringlocationNEW'
+    WHERE warehouse_id = 'insertINTwarehouse_id'
