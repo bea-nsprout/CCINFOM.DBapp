eb03e72a23dd7bf331b2dfc93f8acfee7c06f32d
RD-Tan <ross_tan@dlsu.edu.ph>
2024-11-18 00:25:05 +0800

    some stuff

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 
 const express = require("express");
+const mysql = require("mysql2");
+
 const app = express();
 const port = 3000;
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: process.env.MYSQL_DB_PASSWORD,
+    database: 'warehousedb'
+})
+
+connection.connect()
+
+connection.query('SELECT * FROM item_masterlist', (err, rows, fields) => {
+    // console.log(rows);
+})
 
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!d');
+app.get("api/test", (req, res) => {
+
 })
 
 app.listen(port, () => {
