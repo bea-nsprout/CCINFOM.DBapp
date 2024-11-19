a9c83f10e2ddb9f2c8292fe25ca099aeefc973f8
adlerscodes <150017406+adlerscodes@users.noreply.github.com>
2024-11-19 22:02:38 +0800

    Update request_sql_statements.sql

--- sql/sql_statements_processes/request_sql_statements.sql
+++ sql/sql_statements_processes/request_sql_statements.sql
@@ -1,7 +1,10 @@
 -- REQUEST
 
 -- Create a new request record
-INSERT INTO request ( personnel_id,
+
+/* Have some Function to Prompt the User again is the request_id already exists */ -- [ DONE ]
+INSERT INTO request (request_id, 
+                     personnel_id,
                      date_requested,
                      item_code,
                      qty_balance, 
@@ -10,7 +13,7 @@ INSERT INTO request ( personnel_id,
                      warehouse_from_id, 
                      warehouse_to_id, 
                      status) 
-VALUES (1, '2024-11-01', '0001370000SILV036Y', 100, 100, 'ROLL', 1, 2, 'PENDING');
+VALUES (1, 1, '2024-11-01', '0001370000SILV036Y', 100, 100, 'ROLL', 1, 2, 'PENDING');
  
 -- Modify an existing record
 UPDATE request
@@ -18,32 +21,30 @@ SET item_code = '0000123SDF',
     date_requested = '2024-12-01',
     qty_balance = 101, 
     qty_total = 105, 
-    status = 'PENDING' -- Default of all requests will be pending
-WHERE request_id = 1 AND qty_total - qty_balance > 0; -- request_id is some ID
+    status = 'COMPLETE'
+WHERE qty_total - qty_balance > 0;
 
--- delete existing record
+-- delete existing record -- [ DONE ]
 	DELETE FROM request
-    WHERE request_id = 1 AND qty_balance = 0;
+    WHERE request_id = 1 /* Some request_id */ AND qty_balance = 0; /* ask bea */
     
--- view all request 
+-- view all request -- [ DONE ]
 	-- given a requestID
 	SELECT * 
     FROM request 
     WHERE request_id = 1;
     
-    -- given date requested
+    -- given date requested -- [ DONE ]
     SELECT *
     FROM request
 	WHERE date_requested = '2024-12-01';
     
-    -- given an item code
+    -- given an item code -- [ DONE ]
     SELECT *
     FROM request
-    WHERE item_code = '000000AAAAABBBCCCC';
+    WHERE item_code = 2;
     
--- view all pending requests
+-- view all pending requests -- [ DONE ]
 	SELECT * 
     FROM request
     WHERE status = 'PENDING';
-    
-    
