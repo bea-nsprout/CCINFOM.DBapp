6fa5558847c0d98ee6de34e396b064ec847d6354
beansprout <150442539+bea-nsprout@users.noreply.github.com>
2024-11-20 07:49:15 +0800

    removed redundancy
    removed the ff:
    
    request
    - unit
    - status (should be inferred through conditions > 0 instead)
    
    transfer
    - itemcode
    - warehousefrom
    - warehouseto
    
    production
    - unit

--- sql/sql_statements_for_dbcreation/table_column_creation.sql
+++ sql/sql_statements_for_dbcreation/table_column_creation.sql
@@ -51,10 +51,8 @@ CREATE TABLE request (
     item_code varchar(20) NOT NULL,
     qty_balance int NOT NULL,
     qty_total int NOT NULL,
-    unit varchar(10) NOT NULL,
     warehouse_from_id int NOT NULL,    	-- id of W.house that requests were from
     warehouse_to_id int NOT NULL,		-- id of w.house that requests were sent to
-    status ENUM('PENDING', 'COMPLETE') NOT NULL DEFAULT 'PENDING',
     
     FOREIGN KEY (personnel_id) REFERENCES personnel(personnel_id) ON DELETE RESTRICT, 
     FOREIGN KEY (item_code) REFERENCES item_masterlist(item_code) ON DELETE RESTRICT,
@@ -65,21 +63,14 @@ CREATE TABLE request (
 CREATE TABLE transfer (
 	transfer_id int PRIMARY KEY AUTO_INCREMENT, 
     request_id int NOT NULL, 
-    personnel_id int NOT NULL, 
-    item_code varchar(20) NOT NULL,
+    personnel_id int NOT NULL,
     date_transferred date NOT NULL,
     truck_id varchar(10) NOT NULL,
     quantity int NOT NULL,
-    unit varchar(10) NOT NULL,
-    warehouse_from_id int NOT NULL,    	-- id of W.house that items were from
-    warehouse_to_id int NOT NULL,		-- id of w.house that items were sent to
     
 	FOREIGN KEY (request_id) REFERENCES request(request_id) ON DELETE RESTRICT, 
-    FOREIGN KEY (personnel_id) REFERENCES personnel(personnel_id) ON DELETE RESTRICT, 
-    FOREIGN KEY (item_code) REFERENCES item_masterlist(item_code) ON DELETE RESTRICT, 
-    FOREIGN KEY (truck_id) REFERENCES truck(truck_id) ON DELETE RESTRICT, 
-    FOREIGN KEY (warehouse_from_id) REFERENCES warehouse(warehouse_id) ON DELETE RESTRICT,
-    FOREIGN KEY (warehouse_to_id) REFERENCES warehouse(warehouse_id) ON DELETE RESTRICT
+    FOREIGN KEY (personnel_id) REFERENCES personnel(personnel_id) ON DELETE RESTRICT,
+    FOREIGN KEY (truck_id) REFERENCES truck(truck_id) ON DELETE RESTRICT
 );
 
 CREATE TABLE production (
@@ -87,7 +78,6 @@ CREATE TABLE production (
 	item_code varchar(20) NOT NULL,
     date_produced date NOT NULL,
     qty_produced int NOT NULL,
-    unit varchar(10) NOT NULL,
     warehouse_id int NOT NULL, 
     
     FOREIGN KEY (item_code) REFERENCES item_masterlist(item_code) ON DELETE RESTRICT, 
