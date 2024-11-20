0935b9734b3e52b02d683a49b2edc09fd3023f76
adlerscodes <150017406+adlerscodes@users.noreply.github.com>
2024-11-20 22:39:47 +0800

    Update transfer_sql_statements.sql

--- sql/sql_statements_processes/transfer_sql_statements.sql
+++ sql/sql_statements_processes/transfer_sql_statements.sql
@@ -1,58 +1,51 @@
 -- TRANSFER
--- Insert a new transfer record [ FIXED ]
-	SET @v1 = 90;  -- Amount to transfer
-	SET FOREIGN_KEY_CHECKS = 0;
-	SET @last_transfer_id = LAST_INSERT_ID();
 
+	SET @v1 = 90; -- Amount to transfer
+
+-- create a true or false if the request is valid or not
 	INSERT INTO transfer (request_id, personnel_id, date_transferred, truck_id, quantity)
-	VALUES (@last_transfer_id + 1, 1, CURDATE(), 'WOW', @v1); 
-	/* This makes the transfer_id equal to the request_id */
-    
-	SET FOREIGN_KEY_CHECKS = 1;
-	
-	-- Update the qty_balance in the request table
+	VALUES (1, 1, CURDATE(), 'TRK0001', @v1); 
+	/* Ensure request_id exists in the `request` table */
+
 	UPDATE request r
 	SET r.qty_balance = r.qty_balance - @v1
-	WHERE r.request_id = 17 AND r.qty_balance >= @v1; /* Should be the same transfer_id in transfer table */
+	WHERE r.request_id = 1 /* valid request_id */ AND r.qty_balance >= @v1; 
+	/* Ensure request_id matches the transfer request */
 
-	-- Ask bea how do I link warehouse_inventory to transfer
-	-- Update the warehouse_inventory table for the source warehouse (decrease quantity)
-	-- 	UPDATE warehouse_inventory wi
-	-- 	JOIN transfer t ON t.item_code = wi.item_code
-	-- 	SET wi.quantity = wi.quantity - @v1
-	-- 	WHERE wi.warehouse_id = t.warehouse_from_id
-	--   		AND t.transfer_id = 17; /* Should be the same transfer_id in transfer table */-- 
+-- REVISE THIS !!! with join clause 
+	-- UPDATE warehouse_inventory wi
+	-- JOIN transfer t ON t.item_code = wi.item_code /* Assuming item_code is unique */
+	-- SET wi.quantity = wi.quantity - @v1
+	-- WHERE wi.warehouse_id = t.warehouse_from_id
+	--   AND t.request_id = 1; 
+	/* Link the warehouse inventory and transfer records */
 
--- transfer record
--- SELECT * 
--- FROM transfer;
-
--- request record
--- SELECT * 
--- FROM request;
-
--- warehouse inventory record
--- SELECT * 
--- FROM warehouse_inventory;
-
+	-- SELECT * FROM transfer WHERE request_id = 1;
+	-- SELECT * FROM request WHERE request_id = 1;
+	-- SELECT * FROM warehouse_inventory;
     
 -- Modify an existing record and update qty_balance accordingly [ FIXED ]
-	SET @v1 = 40; -- New transfer quantity
+	SET @v1 = 50; -- New transfer quantity
 	SET @transfer_id = 6; -- Transfer ID to modify
 
-	UPDATE transfer
+	SELECT r.qty_total AS old_qty
+    	FROM request r
+    	WHERE r.request_id = 2;
+    
+   	UPDATE transfer
 	SET quantity = @v1
 	WHERE transfer_id = @transfer_id;
 
 	UPDATE request r
 	JOIN transfer t ON r.request_id = t.request_id
-	SET r.qty_balance = r.qty_total - t.quantity
+	SET r.qty_balance = r.qty_total - 100 /* Old Quantity */ + @v1 /* New Quantity */
 	WHERE t.transfer_id = @transfer_id;
-
-	-- SELECT * FROM transfer WHERE transfer_id = @transfer_id;
-	-- SELECT * FROM request r
-	-- JOIN transfer t ON r.request_id = t.request_id
-	-- WHERE t.transfer_id = @transfer_id;
+    
+-- SELECT * FROM transfer WHERE transfer_id = @transfer_id;
+-- SELECT * FROM request r
+-- JOIN transfer t ON r.request_id = t.request_id
+-- WHERE t.transfer_id = @transfer_id;
+    
   
 -- delete existing record [ FIXED ]
 	-- Update warehouse_inventory to add back the deleted quantity
@@ -65,7 +58,7 @@
 
 	-- Update request to adjust qty_balance
     
-    SET @v1 = 14;
+    SET @v1 = 14; /* transfer_id */
     
 	UPDATE request r
 	JOIN transfer t ON r.request_id = t.request_id
@@ -79,11 +72,13 @@
 
 -- view all request [ FIXED ]
     -- given date transferred
-    SELECT *
+    -- join with request then join with masterlist, display the item description
+	SELECT *
     FROM transfer
 	WHERE date_transferred = '2024-12-01';
     
 -- given an item code [ FIXED ]
+	-- join with request then join with masterlist, display the item description
     SELECT *
     FROM transfer
     WHERE item_code = '09009GFDSG';
