04d437b96621e1ba68d217cab941912dd351bf77
adlerscodes <150017406+adlerscodes@users.noreply.github.com>
2024-11-22 21:39:33 +0800

    Update request_sql_statements.sql

--- sql/sql_statements_processes/request_sql_statements.sql
+++ sql/sql_statements_processes/request_sql_statements.sql
@@ -27,17 +27,20 @@
 -- view all request -- [ DONE ]
 	-- add a join to show the item description, in item_masterlist
 	-- given a requestID
-	SELECT request.*,  
-    FROM request 
-    WHERE request_id = 1;
+	SELECT r.*,  im.item_desc
+    FROM request r
+	JOIN item_masterlist im ON r.item_code = im.item_code  
+    WHERE r.request_id = 1;
     
     -- given date requested -- [ DONE ]
-    SELECT *
-    FROM request
-	WHERE date_requested = '2024-12-01';
+	SELECT r.*, im.item_desc
+    FROM request r
+	    JOIN item_masterlist im ON r.item_code = im.item_code 
+	WHERE r.date_requested = '2023-01-03';
     
     -- given an item code -- [ DONE ]
-    SELECT *
-    FROM request
-    WHERE item_code = 2;
+    SELECT r.*, im.item_desc
+    FROM request r
+	    JOIN item_masterlist im ON r.item_code = im.item_code 
+    WHERE im.item_code = '003200BIG0RBLU036Y';
 
