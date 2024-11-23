d9134031481b893a66870a0a53e2eec9a139740a
beansprout <150442539+bea-nsprout@users.noreply.github.com>
2024-11-23 17:44:36 +0800

    ALL reports done

--- sql/sql_statements_processes/reports_warehouseinventory_sql_statements.sql
+++ sql/sql_statements_processes/reports_warehouseinventory_sql_statements.sql
@@ -1,70 +1,55 @@
 -- REPORT WAREHOUSE INVENTORY, COMPARING 2 WAREHOUSES
 	-- DATE: there's a requirement that we should add the date for each report, so please do that.. like date the report was generated
     -- BODY: comparing 2 warehouses
-	SET @v1 = 4;
-	SET @v2 = 5;
+	SET @v1 = 4;    -- warehouse1 id
+	SET @v2 = 5;    -- warehouse2 id
 
-	SELECT i1.item_code, SUM(i1.quantity) AS First_Total_Sum, SUM(i2.quantity) AS Second_Total_Sum
+	SELECT i.item_code, i.item_desc, SUM(i1.quantity) AS warehouse1, SUM(i2.quantity) AS warehouse2
 	FROM inventories i1
 	JOIN inventories i2 ON i1.item_code = i2.item_code AND i1.warehouse_id != i2.warehouse_id  
 	JOIN warehouses w1 ON i1.warehouse_id = w1.warehouse_id
-	JOIN warehouses w2 ON i2.warehouse_id = w2.warehouse_id 
+	JOIN warehouses w2 ON i2.warehouse_id = w2.warehouse_id
+	JOIN items i ON i1.item_code = i.item_code
 	WHERE i1.warehouse_id = @v1 AND i2.warehouse_id = @v2
 	GROUP BY i1.warehouse_id, i2.warehouse_id, i1.item_code  
-	ORDER BY First_Total_Sum, Second_Total_Sum, i1.item_code;
-		
-	/* If you guys want to sort it by warehouse ID: 
-	ORDER BY i1.warehouse_id, i2.warehouse_id, i1.item_code; */
+	ORDER BY warehouse1 DESC, warehouse2 DESC, i1.item_code;
 
-	-- Count the request of Warehouse FROM id
-	SET @v1 = 4;
-	SET @v2 = 5;
 
-	SELECT COUNT(CASE WHEN i1.warehouse_from_id = @v1 THEN 1 END) AS First_Request_Count,
-	COUNT(CASE WHEN i2.warehouse_from_id = @v2 THEN 1 END) AS Second_Request_Count
-	FROM requests i1
-	JOIN requests i2 ON i1.request_id = i2.request_id
-	WHERE i1.warehouse_from_id = @v1 OR i2.warehouse_from_id = @v2;
 
-	-- Count the request of Warehouse TO id
-	SET @v1 = 4;
-	SET @v2 = 5;
-
-	SELECT COUNT(CASE WHEN i1.warehouse_to_id = @v1 THEN 1 END) AS First_Request_Count,
-	COUNT(CASE WHEN i2.warehouse_to_id = @v2 THEN 1 END) AS Second_Request_Count
-	FROM requests i1
-	JOIN requests i2 ON i1.request_id = i2.request_id
-	WHERE i1.warehouse_to_id = @v1 OR i2.warehouse_to_id = @v2;
-
-	-- Count the transfer of warehouse ID
-	/* ADLER DO THIS TMRR!! */
         
 -- Report the total # of items produced in all warehouses
-	-- past 30 days 
-	SELECT w.warehouse_id, w.warehouse_name,
+    SET @ndays = 1000;
+	SELECT w.warehouse_name, w.location,
 	(
         	SELECT SUM(p.qty_produced) 
         	FROM productions p 
-        	WHERE p.warehouse_id = w.warehouse_id AND p.date_produced BETWEEN (CURDATE() - INTERVAL 30 DAY) AND CURDATE()
+        	WHERE p.warehouse_id = w.warehouse_id AND p.date_produced BETWEEN (CURDATE() - INTERVAL @ndays DAY) AND CURDATE()
     	) AS Total_Produced_Past_30_Days
 
 	FROM warehouses w
 	ORDER BY w.warehouse_id;
 
-	-- past 365 days 
-	SELECT w.warehouse_id, w.warehouse_name,
-     	(
-	        SELECT SUM(p.qty_produced) 
-        	FROM productions p 
-       		WHERE p.warehouse_id = w.warehouse_id AND p.date_produced BETWEEN (CURDATE() - INTERVAL 365 DAY) AND CURDATE()
-    	) AS Total_Produced_Past_Year 
-	
-    	FROM warehouses w
-	ORDER BY w.warehouse_id;
 
+-- REQUEST REPORT: number of requests from past n days
+    SET @ndays = 1000;
 
-
-
+    SELECT warehouse_name, location,
+           IF(reqs_received is null, 0, reqs_received) AS reqs_received,
+           IF(reqs_sent is null, 0, reqs_sent) AS reqs_sent
+    FROM warehouses w
+    LEFT JOIN (SELECT warehouse_id, COUNT(*) AS reqs_received
+            FROM requests r
+            JOIN warehouses w ON warehouse_to_id = warehouse_id
+            WHERE date_requested BETWEEN (CURDATE() - INTERVAL @ndays DAY) AND CURDATE()
+            GROUP BY warehouse_to_id) AS received
+        ON received.warehouse_id = w.warehouse_id
+    LEFT JOIN ( SELECT warehouse_id, COUNT(*) AS reqs_sent
+        FROM requests r
+        JOIN warehouses w ON warehouse_from_id = warehouse_id
+        WHERE date_requested BETWEEN (CURDATE() - INTERVAL @ndays DAY) AND CURDATE()
+        GROUP BY warehouse_from_id) AS delivered
+        ON delivered.warehouse_id = w.warehouse_id
+    ORDER BY w.warehouse_id DESC;
 
 
 -- TRANSFER REPORT: number of transfers from past n days
@@ -89,4 +74,14 @@
         GROUP BY warehouse_from_id) AS delivered
         ON delivered.warehouse_id = w.warehouse_id
     ORDER BY w.warehouse_id DESC;
+
+-- ADJUSTMENT REPORT:
+    SET @ndays = 1000;
+
+    SELECT w.warehouse_name, w.location, COUNT(qty_adjusted) AS times_adjusted, SUM(ABS(a.qty_adjusted)) AS total_adjusted
+    FROM adjustments a
+    JOIN warehouses w ON w.warehouse_id = a.warehouse_id
+    WHERE DATE(time_log) BETWEEN (CURDATE() - INTERVAL @ndays DAY) AND CURDATE()
+    GROUP BY a.warehouse_id;
+
 
